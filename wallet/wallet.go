09eb389fdbcd5733850414bdd33fcef5c45f591a
Caleb James DeLisle <cjd@cjdns.fr>
2019-08-20 17:03:39 +0200

    Always use the libbtcwallet chain package when possible

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2200,9 +2200,9 @@ func (w *Wallet) GetTransactions(startBlock, endBlock *BlockIdentifier, cancel <
 				return nil, errors.New("no chain server client")
 			}
 			switch client := chainClient.(type) {
-			case *chain.RPCClient:
+			case *mychain.RPCClient:
 				startResp = client.GetBlockVerboseTxAsync(startBlock.hash)
-			case *chain.BitcoindClient:
+			case *mychain.BitcoindClient:
 				var err error
 				start, err = client.GetBlockHeight(startBlock.hash)
 				if err != nil {
